bf92846fe07316ef15df8a420f72092d612bd3fa
Isaac Rossi <isaacjohnrossi@gmail.com>
2021-09-11 16:13:54 +1000

    created our svg element and applied some text elements for the axes, a players group, and a circlewithin the players group for the data viz

--- data.js
+++ data.js
@@ -1,6 +1,30 @@
 const apiHost = "api-football-v1.p.rapidapi.com/v3"
 const apiKey = "ed258403cfmsh30f5a085abc2d2ep1858afjsnf5d899d5c50c"
 
+// grab the svg tag from index.html
+const svg = d3.select("svg")
+
+// apply a width and height to the svg
+svg
+    .attr("width", "960")
+    .attr("height", "720")
+
+// creating a text element within the svg for the x-axis
+    const axisXText = svg 
+    .append("text")
+    .attr("class", "x-axis")
+    .attr("transform", "translate(480, 670)")
+    .text("x-axis")
+
+// creating a text element within the svg for the y-axis    
+const axisYText = svg 
+    .append("text")
+    .attr("class", "y-axis")
+    .attr("transform", "translate(30, 360) rotate(-90)")
+    .text("y-axis")    
+
+
+// fetching a response from api-football.
 const getStats = function (playerId) {
     return fetch(`https://api-football-v1.p.rapidapi.com/v3/players?id=${playerId}&season=2021`, {
         "method": "GET",
