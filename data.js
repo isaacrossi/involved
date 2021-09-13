c4f406eac7a6267cc0957ee0bf3c07fcae588ad9
Isaac Rossi <isaacjohnrossi@gmail.com>
2021-09-13 18:07:26 +1000

    began writing some code to display the photos in a circle

--- data.js
+++ data.js
@@ -4,6 +4,7 @@ const apiKey = "ed258403cfmsh30f5a085abc2d2ep1858afjsnf5d899d5c50c"
 // grab the svg tag from index.html
 const svg = d3.select("svg")
 
+
 // apply a width and height to the svg
 svg
     .attr("width", "960")
@@ -14,28 +15,28 @@ svg
     .append("text")
     .attr("class", "x-axis")
     .attr("transform", "translate(480, 670)")
-    .text("x-axis")
+    .text("Number of minutes played")
 
 // creating a text element within the svg for the y-axis    
 const axisYText = svg 
     .append("text")
     .attr("class", "y-axis")
     .attr("transform", "translate(30, 360) rotate(-90)")
-    .text("y-axis")    
+    .text("Total number of goals")    
 
 
 // fetching a response from api-football.
 const getStats = function () {
 
-    let valueX = "goals"
-    let valueY = "apps"
+    let valueX = "minutes"
+    let valueY = "goals"
 
     const scaleX = d3.scaleLinear()
-        .domain([0, 20])
+        .domain([0, 500])
         .range([100, 860])
 
     const scaleY = d3.scaleLinear()
-        .domain([0, 20])
+        .domain([0, 30])
         .range([620, 100])
     
 
@@ -47,45 +48,66 @@ const getStats = function () {
         }
     })
 
-    // getting the total goals and appearences from the fetch request
+    // getting the total goals, shots, name, and photo from the fetch request
     .then(response => response.json()) 
     .then( data => {
         return data.response.map(response => {
             data = response.statistics.map( statistic => {
                 return {
                     goals: statistic.goals.total,
-                    apps: statistic.games.appearences,
+                    minutes: statistic.games.minutes,
                     name: response.player.name,
                     photo: response.player.photo
                 }
             })
 
-            console.log(data)
-
             // creating players groups within our svg whose positions are decided by the
             // data given back from the API
-            const players = svg 
+
+            
+
+
+            console.log(response.player.photo)
+            
+            const players = svg
                 .selectAll("g.players")
                 .data(data, (d, i) => { return d.name})
                 .enter()
                 .append("g")
                 .attr("class", "players")
                 .attr("transform", (d, i) => {
-                    const x = scaleX(d.goals)
-                    const y = scaleY(d.apps)
+                    const x = scaleX(d.minutes)
+                    const y = scaleY(d.goals)
                     return `translate(${x}, ${y})`
                 })
+            
+            const defs = players
+                .append("defs")
 
-            // creating a circle element within the players group for the visualisation
+            defs
+                .selectAll("patterns")
+                .data(data)   
+                .enter() 
+                .append("pattern")
+                .attr("id", "image")
+                // d.photo returns the player URL eg: https://media.api-sports.io/football/players/897.png
+                .attr("xlink:href", (d, i) => { return d.photo })
+                .attr("x", 0)
+                .attr("y", 0)
+                .attr("width", 40)
+                .attr("height", 40)
+ 
             players
-                .append("image")
-                .attr("class", "photo")
-                .attr("xlink:href", (d, i) => {return `${d.photo}`})
-                .attr("x", "0")
-                .attr("y", "0")
-                .attr("width", "50")
-                .attr("height", "50")
-                
+                .append("circle")
+                .attr("cx", 0)
+                .attr("cy", 0)
+                .attr("r", 0)
+                .transition()
+                .attr("r", 15)
+                .style("fill", "transparent")       
+                .style("stroke", "black")     
+                .style("stroke-width", 0.25)
+                .style("fill", "url(#image)")
         })
         
     })
@@ -93,6 +115,7 @@ const getStats = function () {
 }
 
 getStats()
+
 
 
  
